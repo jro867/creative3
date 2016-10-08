7add4b7810aead11245bccbb9e6fe9165b98d13e
Jesus Ramos <jro867@gmail.com>
2016-10-08 11:52:46 -0600

    connecting to db and pulling talks, no working yet

--- script.js
+++ script.js
@@ -1,39 +1,113 @@
+var database = "";
+init();
 
-var app = angular.module('',[]);
+function init(){
 
-app.controller('searchController',function($scope, jobService){
+	var mongoClient = require('mongodb').MongoClient;
+	var prefix = 'mongodb://';
+	var db = 'main';
+	var url = prefix + 'localhost:27017' + '/' + db;
 
-});
+	mongoClient.connect(url, function(err,db){
+		if(err){
+			throw "Unable to connect to the server. Error: " + err;
+
+		}else{
+			console.log("connected");
+			dataBase = db;
+			pullTalks();
+		} 
+	});
+}
+
+function pullTalks(){
+
+	var cursor = getDataBase().collection('talks').find({}, {_id:0});
+	cursor.each(function(err,doc){
+		if(!err && doc !== null){
+			console.dir(doc);
+			if(smtpPatterns.get(doc.Solution) == undefined){
+				smtpPatterns.set(doc.Solution, []);
+			}
+			smtpPatterns.get(doc.Solution).push(doc.RegEx); 
+		} 
+
+	});
+}
+
+function getDataBase(){
+	return database;
+}
+
+// angular.module('News', ['ui.router'])
+// .controller('MainCtrl', [
+//   '$scope',
+//   'postFactory',
+//   function($scope, postFactory){
+//     $scope.posts = postFactory.posts;
+// //      $scope.addPost = function() {
+// //       $scope.posts.push({title:$scope.formContent,upvotes:0});
+// //       $scope.formContent='';
+// //     };
+//     $scope.addPost = function(){
+//     if($scope.formContent === '') { return; }
+//     $scope.posts.push({
+//       title: $scope.formContent,
+//       upvotes: 0,
+//       comments: []
+//     });
+//     $scope.title = '';
+//   };
+//      $scope.incrementUpvotes = function(post) {
+//       post.upvotes += 1;
+//     };
+//   }
+// ])
 
 
-app.factory('jobService', function($http, $q){
-    return {
-        getJobs: function(url){
-        	console.log("URL: " + url);
-            // var url = "https://api.usa.gov/jobs/search.json?nursing+in+Colorado";
-            return $.getJSON(url,function(data){	        
-            	return data;           
-        	});
-        }
-    };
-});
+// .controller('PostCtrl', [
+//   '$scope',
+//   '$stateParams',
+//   'postFactory', 
+//   function($scope, $stateParams, postFactory){
+//     $scope.post = postFactory.posts[$stateParams.id];
+//     $scope.addComment = function(){
+//       if($scope.body === '') { return; }
+//       $scope.post.comments.push({
+//         body: $scope.body,
+//         upvotes: 0
+//       });
+//       $scope.body = '';
+//     };
+//   $scope.incrementUpvotes = function(comment){
+//     comment.upvotes += 1; 
+//   };
+// }])
 
-function parseQueryString(queryString){
+// .factory('postFactory', [function(){
+//   var o = {
+//     posts: []
+//   };
+//   return o;
+// }])
 
-	var queries = queryString.split(" ");
-	var params = "";
-	var queryLength = queries.length;
-
-	if(queryLength > 1){
-		for(var i=0; i < queryLength; i++){
-			params += "+"+queries[i];
-		}
-		return params;
-	}else{
-		return queryString;
-	}	
-};
-
+// .config([
+//   '$stateProvider',
+//   '$urlRouterProvider',
+//   function($stateProvider, $urlRouterProvider) {
+//     $stateProvider
+//       .state('home', {
+//         url: '/home',
+//         templateUrl: '/home.html',
+//         controller: 'MainCtrl'
+//       })
+//     .state('posts', {
+//         url: '/posts/{id}',
+//         templateUrl: '/posts.html',
+//         controller: 'PostCtrl'
+//       });
+//     $urlRouterProvider.otherwise('home');
+// }])
 
 $(document).ready(function(){
 
